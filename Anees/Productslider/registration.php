7305ae7bb7d58d39112f3930c2e5b5c7193a7c14
aneesahmadweb <aneesahmadweb@gmail.com>
2018-01-19 02:20:58 -0800

    Update registration.php

--- Anees/Productslider/registration.php
+++ Anees/Productslider/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Genmato_Sample',
+    'Anees_Productslider',
     __DIR__
 );
